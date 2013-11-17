b25daec3919703c4ebdc8a272a1905193b4faf8c
Martin <martirj@ifi.uio.no>
2013-11-17 18:04:27 +0100

    Replaced calls to strnlen() with calls to ruter_strnlen().

--- src/include/ruter/util.h
+++ src/include/ruter/util.h
@@ -5,4 +5,6 @@ void ruter_safe_free(void *ptr);
 
 char *ruter_strndup(char const *s, size_t n);
 
+size_t ruter_strnlen(const char *s, size_t maxlen);
+
 #endif
--- src/ruter/util.c
+++ src/ruter/util.c
@@ -11,7 +11,7 @@ void ruter_safe_free(void *ptr)
 
 char *ruter_strndup(char const *s, size_t n)
 {
-	size_t len = strnlen (s, n);
+	size_t len = ruter_strnlen(s, n);
 	char *dup = malloc(len + 1);
 	
 	if (NULL == dup) {
@@ -21,4 +21,14 @@ char *ruter_strndup(char const *s, size_t n)
 	dup[len] = '\0';
 	
 	return memcpy(dup, s, len);
+}
+
+size_t ruter_strnlen(const char *s, size_t maxlen)
+{
+	register const char *e;
+	size_t n;
+	
+	for (e = s, n = 0; '\0' != *e && n < maxlen; e++, n++);
+	
+	return n;
 }
