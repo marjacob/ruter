a3283dfed6fbcd16cacff1444517fb5ec03b62b5
Martin <martirj@ifi.uio.no>
2014-02-02 15:14:15 +0100

    Added kernel-doc.

--- src/include/ruter/constants.h
+++ src/include/ruter/constants.h
@@ -1,6 +1,8 @@
 #ifndef __H_RUTER_CONSTANTS__
 #define __H_RUTER_CONSTANTS__
 
+/* Initial size of the data receive buffer. 
+ * The buffer size will be increased automatically when necessary. */
 #ifndef RUTER_BUFFER_SIZE
 #	define RUTER_BUFFER_SIZE 8192
 #endif
@@ -19,6 +21,15 @@
 
 /* See http://labs.ruter.no/how-to-use-the-api/about-the-data-model.aspx */
 
+/**
+ * enum place_type - Various kinds of stops.
+ * @PT_STOP:	A regular stop for public transportation.
+ * @PT_AREA:	An area that may contain stops.
+ * @PT_POI:	A point of interest that may have stops nearby.
+ * @PT_STREET:	A street that may have stops nearby.
+ *
+ * These are the various kinds of stops available through the API.
+ */
 enum place_type {
 	PT_STOP = 0,
 	PT_AREA = 1,
@@ -26,6 +37,21 @@ enum place_type {
 	PT_STREET = 3
 };
 
+/**
+ * enum transport_type - Various methods of transportation.
+ * @TT_WALKING:		Usually indicates a short walk between stops.
+ * @TT_AIRPORT_BUS:	Transport to an airport available by bus.
+ * @TT_BUS:		Transport available by bus.
+ * @TT_DUMMY:		Probably used internally by the servers.
+ * @TT_AIRPORT_TRAIN:	Transport to an airport available by train.
+ * @TT_BOAT:		Transport available by boat.
+ * @TT_TRAIN:		Transport available by train.
+ * @TT_TRAM:		Transport available by tram.
+ * @TT_METRO:		Transport available by metro.
+ *
+ * These are the various methods of transportation available trough the 
+ * travel planner API.
+ */
 enum transport_type {
 	TT_WALKING = 0,
 	TT_AIRPORT_BUS = 1,
@@ -38,6 +64,17 @@ enum transport_type {
 	TT_METRO = 8
 };
  
+/**
+ * enum vehicle_mode - Various methods of transportation.
+ * @VM_BUS:	Realtime information available for bus.
+ * @VM_FERRY:	Realtime information available for ferry.
+ * @VM_RAIL:	Realtime information available for rail.
+ * @VM_TRAM:	Realtime information available for tram.
+ * @VM_METRO:	Realtime information available for metro.
+ *
+ * These are the various methods of transportation available through the 
+ * realtime systems API.
+ */
 enum vehicle_mode {
 	VM_BUS = 0,
 	VM_FERRY = 1,
